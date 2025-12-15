421fac06a39a4fc25d0245672b2d0430a31bea57
hessel <vgerlachhessel@gmail.com>
2025-12-14 23:56:19 -0300

    add: springdoc configuration

---
+++ src/main/java/com/wishlist/config/OpenApiConfig.java
@@ -0,0 +1,19 @@
+package com.wishlist.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI wishlistOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Wishlist API")
+                        .description("API for managing customer wishlists")
+                        .version("v1"));
+    }
+}
